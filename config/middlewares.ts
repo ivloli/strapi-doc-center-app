f43fa38e1338dcde17bd165022bb042281852765
ivloli <343900695@qq.com>
2026-04-18 17:00:02 +0800

    fix cors and some policy

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,8 +3,26 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      referrerPolicy: {
+        policy: 'no-referrer-when-downgrade',
+      },
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', '*'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
