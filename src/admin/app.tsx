fbd7eef3c1c20a3bcfe0261187b21f33e4cdfd43
ivloli <343900695@qq.com>
2026-04-18 18:47:27 +0800

    manual translate cn

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,8 +1,12 @@
 import type { StrapiApp } from '@strapi/strapi/admin';
+import zhHansTranslations from './extensions/translations/zh-Hans.json';
 
 export default {
   config: {
     locales: ['zh-Hans'],
+    translations: {
+      'zh-Hans': zhHansTranslations,
+    },
   },
   bootstrap(_app: StrapiApp) {},
 };
