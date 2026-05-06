ef335abe4011669c8a853473d7e1deadd918d5bf
ivloli <343900695@qq.com>
2026-05-06 21:36:56 +0800

    update cors and nginx https config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,34 +1,45 @@
 import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      referrerPolicy: {
-        policy: 'no-referrer-when-downgrade',
-      },
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'img-src': ["'self'", 'data:', 'blob:', '*'],
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => {
+  const corsOrigins = env.array('CORS_ORIGIN', [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+    'http://127.0.0.1:4173',
+    'http://localhost:4173',
+  ]);
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        referrerPolicy: {
+          policy: 'no-referrer-when-downgrade',
+        },
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'img-src': ["'self'", 'data:', 'blob:', '*'],
+          },
         },
       },
     },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: '*',
+    {
+      name: 'strapi::cors',
+      config: {
+        origin: corsOrigins,
+      },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
 
 export default config;
