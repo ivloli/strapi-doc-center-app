0131ebf0bf14f4f3c17f1f4b5642d9a4be1deb4b
ivloli <343900695@qq.com>
2026-06-17 19:06:36 +0800

    fix: max pageSize->5000

--- config/api.ts
+++ config/api.ts
@@ -3,7 +3,7 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Api = {
   rest: {
     defaultLimit: 25,
-    maxLimit: 100,
+    maxLimit: 5000,
     withCount: true,
   },
 };
